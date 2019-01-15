5530323fa238d8b27d558d8b47e18b430382eccb
Nick <45113563+NightWolf1038@users.noreply.github.com>
2019-01-15 13:58:33 -0300

    Support for more currencies

--- SteamStoreQuery/SteamStoreQuery/Query.cs
+++ SteamStoreQuery/SteamStoreQuery/Query.cs
@@ -7,11 +7,11 @@ namespace SteamStoreQuery
 {
     public static class Query
     {
-        public static List<Listing> Search(string gameName)
+        public static List<Listing> Search(string gameName, string cc)
         {
             List<Listing> results = new List<Listing>();
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-            string response = new WebClient().DownloadString($"https://store.steampowered.com/search/suggest?term={gameName}&f=games&cc=us&lang=english&v=2286217");
+            string response = new WebClient().DownloadString($"https://store.steampowered.com/search/suggest?term={gameName}&f=games&cc={cc}&lang=english&v=2286217");
             if (!response.Contains("match ds_collapse_flag "))
                 return results; 
 
