a5a99b6182c1e3f3953a87c330663567c4cfb8e5
Cole <swiftyspiffy@gmail.com>
2021-01-11 22:07:51 -0800

    fix parsing bugs

--- SteamStoreQuery/SteamStoreQuery/Listing.cs
+++ SteamStoreQuery/SteamStoreQuery/Listing.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Globalization;
+using System.Text;
 using SteamStoreQuery.Enums;
 
 namespace SteamStoreQuery
@@ -8,34 +10,84 @@ namespace SteamStoreQuery
         public string Name { get; protected set; }
         public string StoreLink { get; protected set; }
         public string ImageLink { get; protected set; }
-        public string Price { get; protected set; }
+        public double? Price { get; protected set; }
         public sType SaleType { get; protected set; }
-        public int AppId { get; protected set; }
+        public string AppId { get; protected set; }
 
         public Listing(string listingData)
         {
-            Name = listingData.Split('>')[2].Split('<')[0];
-            StoreLink = listingData.Split('=')[4].Replace("\"", "").Split('?')[0];
-            AppId = int.Parse(listingData.Split('=')[1].Replace("\"", "").Split(' ')[0]);
-            ImageLink = listingData.Split('>')[4].Replace("\"", "").Split('=')[1];
-            if (ImageLink.Contains("?"))
-                ImageLink = ImageLink.Split('?')[0];
-            string priceCandidate = listingData.Split('>')[7].Split('<')[0];
+            Name = handleSpecialCharacters(listingData.Split('>')[2].Split('<')[0]);
+            StoreLink = handleSpecialCharacters(getStoreLink(listingData));
+            AppId = handleSpecialCharacters(getAppId(listingData));
+            ImageLink = handleSpecialCharacters(getImageLink(listingData));
+            var pricingData = getPricing(listingData);
+            SaleType = pricingData.Item1;
+            Price = pricingData.Item2;
+        }
+
+        private static string getName(string input)
+        {
+            //<div class="match_name">
+            return input.Split(new string[] { "<div class=\"match_name\">" }, StringSplitOptions.None)[1].Split(new string[] { "</div>" }, StringSplitOptions.None)[0];
+        }
+
+        private static string getStoreLink(string input)
+        {
+            var first = input.Split(new string[] { "data-ds-tagids=\"" }, StringSplitOptions.None)[1];
+            var second = first.Split(new string[] { "\"><div class=\"match_name\"" }, StringSplitOptions.None)[0];
+            var final = second.Split(new string[] { "href=\"" }, StringSplitOptions.None)[1];
+            if (final.Contains("?"))
+            {
+                final = final.Split('?')[0];
+            }
+            if(final.EndsWith("/"))
+            {
+                final = final.TrimEnd('/');
+            }
+
+            return final;
+        }
+
+        private static string getAppId(string input)
+        {
+            return input.Split('=')[1].Replace("\"", "").Split(' ')[0];
+        }
+
+        private static string getImageLink(string input)
+        {
+            var imageLink = input.Split('>')[4].Replace("\"", "").Split('=')[1];
+            if (imageLink.Contains("?"))
+                imageLink = imageLink.Split('?')[0];
+            return imageLink;
+        }
+
+        private static Tuple<sType, double?> getPricing(string input)
+        {
+            string priceCandidate = input.Split('>')[7].Split('<')[0];
+            double? price = null;
+            sType saleType = sType.NotAvailable;
             if (priceCandidate == null || priceCandidate.Length < 2)
             {
-                Price = null;
-                SaleType = sType.NotAvailable;
+                price = null;
+                saleType = sType.NotAvailable;
             }
-            else if(priceCandidate.ToLower().Contains("free"))
+            else if (priceCandidate.ToLower().Contains("free"))
             {
-                Price = "Free";
-                SaleType = sType.FreeToPlay;
+                price = null;
+                saleType = sType.FreeToPlay;
             }
             else
             {
-                Price = priceCandidate;
-                SaleType = sType.CostsMoney;
+                price = double.Parse(priceCandidate.Replace("$", ""), CultureInfo.InvariantCulture);
+                saleType = sType.CostsMoney;
             }
+            return new Tuple<sType, double?>(saleType, price);
+        }
+
+        private static string handleSpecialCharacters(string input)
+        {
+            var bytes = Encoding.Default.GetBytes(input);
+            return Encoding.UTF8.GetString(bytes);
         }
     }
 }
